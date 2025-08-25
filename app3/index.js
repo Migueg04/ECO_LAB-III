21f79cb71c2a1f85d9b541138ba17e11c8f784ee
Edward Figueroa <edwardfigueroavelasco@gmail.com>
2025-08-25 07:13:51 -0500

    muli-clients code base

---
+++ app3/index.js
@@ -0,0 +1,8 @@
+document.getElementById("get-btn").addEventListener("click", getUsers);
+
+function getUsers() {
+  fetch("http://localhost:5050/users")
+    .then((response) => response.json())
+    .then((data) => console.log("get response", data))
+    .catch((error) => console.error("Error:", error));
+}
